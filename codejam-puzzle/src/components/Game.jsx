aaf6a5802d1bacc5af53e4dc928bd077bf0ba5f1
makshorror <makshorror96@gmail.com>
2022-10-24 16:11:46 +0300

    feat: add size fn and timer

--- codejam-puzzle/src/components/Game.jsx
+++ codejam-puzzle/src/components/Game.jsx
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+
+alert('Привет, Уважаемый Проверяющий! Я не успел еще доделать работу, Прошу пожалуйста проверь через 2 дня(26 октября вечером). Спасибо огромное за понимание! Если что связь: ТГ: @makshorror Или Discord: gangbang69#2015')
 
 let id = 16;
 
@@ -7,8 +9,6 @@ let gemArr = [];
 let style = {gridTemplateColumns: '1fr 1fr 1fr 1fr'}
 
 let fontSize = {fontSize: '50px'}
-
-let activeBtn = {}
 
 let anim = {}
 
@@ -26,58 +26,70 @@ function createRandom(x){
     gemArr.splice(index, 1, '')
 }
 
-
 function Game (){
+
     const createBlocks = () => {
         let items = [];
         for(let i=0;i< gemArr.length;i++){
 
             if (gemArr[i] === ''){
-                items.push(<div key={gemArr[i]} className={'empty'}>{gemArr[i]}</div>)
+                items.push(<div key={gemArr[i]} className={'empty'} id={'empty'}>{gemArr[i]}</div>)
             } else {
-                items.push(<div key={gemArr[i]} className={'gem'} style={fontSize}>{gemArr[i]}</div>)
+                items.push(<div key={gemArr[i]} id={gemArr[i]} className={'gem'} style={fontSize} onClick={gemMove}>{gemArr[i]}</div>)
             }
         }
         return items
     }
-    let [moves, setMoves] = useState(0);
-    let [sizes, setSize] = useState('4x4');
-    let [gems, setGems] = useState('')
-    let [run, setRun] = useState('00:00')
-    let [flag, setFlag] = useState(false)
-    let min = 0
-    let sec = 0
-    let interval;
 
-    function stopWatch(x){
-        setFlag(flag = x)
-        setRun(run = '00:00')
-        if (flag === false){
-            setFlag(flag = true)
-            interval = setInterval(()=>{
-                sec += 1;
-                if (sec === 60){
-                    sec = 0;
-                    min += 1;
-                }
-                if (sec < 10 && min < 10){
-                    setRun(run = '0' + min + ':0' + sec)
-                } else if ((sec >= 10 && min < 10)){
-                    setRun(run = '0' + min + ':' + sec)
-                } else if((sec >= 10 && min >= 10)){
-                    setRun(run = min + ':' + sec)
-                }
+    function gemMove(e){
 
-            }, 1000)
-        } else if(flag === true){
-            setFlag(flag = false)
+
+        let emptyIndex = gemArr.indexOf('')
+        let elem = parseInt(e.target.id,10)
+        let index = gemArr.indexOf(elem)
+        win()
+        if (gemArr[index - 1] === '' || gemArr[index + 1] === '' || gemArr[index - Math.sqrt(id)] === '' || gemArr[index + Math.sqrt(id)] === ''){
+
+            [gemArr[index], gemArr[emptyIndex]] = [gemArr[emptyIndex], gemArr[index]];
+            [gems[index], gems[emptyIndex]] = [gems[emptyIndex], gems[index]];
+
+            setMoves(moves += 1)
+
+
         }
+
+
 
 
     }
 
+    function win(){
+        const sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, '', 15]
+        if (gemArr === sorted){
+            alert('Win')
+        } else {
+            console.log(gemArr)
+        }
+
+    }
+
+    let [stopBtn, setStop] = useState('stop')
+    let [moves, setMoves] = useState(0);
+    let [sizes, setSize] = useState('4x4');
+    let [gems, setGems] = useState(null)
+    let [run, setRun] = useState('00:00')
+    let min = 0;
+    let sec = 0;
+    let move = 0;
+    const interval = React.useRef(null);
+    // 'Choose Frame Size and click "Shuffle and start"! And Good Luck :)'
+
     function stop(){
-        stopWatch(true)
+        clearInterval(interval.current)
+        interval.current = null;
+        setStop(stopBtn = 'stop')
+        let info = 'Your time: ' + run +  '\nYour move: ' + moves
+        setGems(gems = info)
     }
     function x3(){
         setSize(sizes = '3x3')
@@ -138,11 +150,28 @@ function shuffle(){
     if (id === 64){
         x8()
     }
-    activeBtn = {backgroundColor: '#bbbbbb', color: 'black'}
+    clearInterval(interval.current)
+    interval.current = null;
+    setMoves(moves = 0)
     anim = {animation: 'none'}
+    setStop(stopBtn = 'stopActive')
     setGems(gems = createBlocks())
-    stopWatch(false)
+    interval.current = setInterval(()=>{
+        sec += 1;
+        if (sec === 60){
+            sec = 0;
+            min += 1;
+        }
+        if (sec < 10 && min < 10){
+            setRun(run = '0' + min + ':0' + sec)
+        } else if ((sec >= 10 && min < 10)){
+            setRun(run = '0' + min + ':' + sec)
+        } else if((sec >= 10 && min >= 10)){
+            setRun(run = min + ':' + sec)
+        }
+    }, 1000)
 }
+
 
 
     return (
@@ -152,7 +181,7 @@ function shuffle(){
                     <button id="shuffle" style={anim} onClick={shuffle}>
                         Shuffle and start
                     </button>
-                    <button id="stop" style={activeBtn} onClick={stop}>
+                    <button id={stopBtn} onClick={stop}>
                         Stop
                     </button>
                     <button id="save">
